e486b82e16a4f19c23b3e8053727727e7abf9b31
Kura <kura@deviling.net>
2011-12-24 22:53:44 +0000

    Added an index page that works

--- src/core/server.py
+++ src/core/server.py
@@ -1,5 +1,10 @@
+import datetime
 from log import log
+from routing import ROUTES
+from status import STATUS_CODES
+from regexes import HTTP10_REGEX
 
+now = datetime.datetime.now()
 
 def handle(sock, addr):
     fd = sock.makefile("rw")
@@ -7,8 +12,13 @@ def handle(sock, addr):
         line = fd.readline()
         if not line:
             break
+        if HTTP10_REGEX.match(line):
+            log.debug("HTTP/1.0")
+            fd.write("HTTP/1.1 500 %s" % STATUS_CODES['1.1'][505])
         if line.lower().startswith("get"):
-            fd.write(200)
-            fd.write(open("index.html").read())
+            fd.write("HTTP/1.1 200 %s\n" % STATUS_CODES['1.1'][200])
+        fd.write("Server: Asynchronous Python HTTP Daemon\n")
+        fd.write("Date: %s\n" % now.strftime("%a, %m %b %Y %H:%M:%S %Z"))
         log.debug(line)
-
+        fd.write("\n\nHi Kura")
+        return
--- src/regexes.py
+++ src/regexes.py
@@ -2,3 +2,4 @@ import re
 
 
 CONFIG_REGEX = re.compile(r"(?P<name>.*?)\s*[:=]\s*(?P<value>.*)")
+HTTP10_REGEX = re.compile(r"(.*)HTTP\/1\.0")
